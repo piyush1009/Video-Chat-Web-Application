aca640432e9a677e832e4df793e098b9c51e2ae4
Piyush Khandelwal <piyushkhandelwal0910@gmail.com>
2020-08-23 18:01:01 +0530

    commits done for day

--- public/script.js
+++ public/script.js
@@ -1,5 +1,10 @@
 const socket=io('/') //reference to socket and connecting a path
 
+const myPeer = new Peer(undefined, { //undefined because we are let the server take care of generating our own id.
+    host: '/',
+    port: '3003'
+});
+
 socket.emit('join-room',ROOM_ID,10)  //sent event to server
 socket.on('user-connected',userId => {  //Listening to user connected message
     console.log('User connected'+userId);
