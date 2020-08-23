a0935d9100861fa7c3bc4c255d12884dcf83ced7
Piyush Khandelwal <piyushkhandelwal0910@gmail.com>
2020-08-23 13:30:27 +0530

    commit

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express=require('express');
 const app=express();
 const server=require('http').Server(app);//create a server based on express server and then passes it to socket.io
 const io=require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid')   //for dynamic url
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
 
 app.get('/',(req,res) => {
-
+    res.redirect(`/${uuidV4()}`)  //Pass to room
 });
 
-app.get('/:room', (req, res) => {
+app.get('/:room', (req, res) => {   //room is dynamic parameter
     res.render('room', { roomId: req.params.room })
 });
 
